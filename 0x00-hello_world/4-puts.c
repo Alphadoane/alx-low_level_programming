167a67ab07cd1ed9e2af8b1bf8991f2097c2fbee
Alphadoane <superalphablood@gmail.com>
2023-08-10 04:06:39 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle,");
-    return 0;
+int main(void)
+{
+printf "Programming is like building a multilingual puzzle ;
+return 0;
 }
