f259d190b97c60f08a122bcfede1ea4438b3b3f1
Alphadoane <superalphablood@gmail.com>
2023-08-11 05:08:29 -0700

    s

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,18 +9,16 @@
  */
 int main(void)
 {
-putchar;[
-('0' + 0);
-('0' + 1);
-('0' + 2);
-('0' + 3);
-('0' + 4);
-('0' + 5);
-('0' + 6);
-('0' + 7);
-('0' + 8);
-('0' + 9);
-]
+putchar('0' + 0);
+putchar('0' + 1);
+putchar('0' + 2);
+putchar('0' + 3);
+putchar('0' + 4);
+putchar('0' + 5);
+putchar('0' + 6);
+putchar('0' + 7);
+putchar('0' + 8);
+putchar('0' + 9);
 putchar('\n');
 
 return (0);
