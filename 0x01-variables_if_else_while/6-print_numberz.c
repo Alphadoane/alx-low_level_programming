1f118d8954eaeb3623628ff5e585b05f140187e7
Alphadoane <superalphablood@gmail.com>
2023-08-11 04:46:02 -0700

    s

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all single-digit numbers of base 10,
+ *              starting from 0, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar('0' + 0);
+putchar('0' + 1);
+putchar('0' + 2);
+putchar('0' + 3);
+putchar('0' + 4);
+putchar('0' + 5);
+putchar('0' + 6);
+putchar('0' + 7);
+putchar('0' + 8);
+putchar('0' + 9);
+
+putchar('\n');
+
+return (0);
+}
