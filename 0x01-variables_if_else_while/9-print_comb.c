fc8b310db084cffd3b40b42bbf1e5864ad10f587
Alphadoane <superalphablood@gmail.com>
2023-08-11 05:25:13 -0700

    d

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all possible combinations of single-digit numbers,
+ *              separated by a comma and a space, in ascending order.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit1;
+int digit2;
+
+for (digit1 = 0; digit1 <= 9; digit1++)
+{
+for (digit2 = digit1; digit2 <= 9; digit2++)
+{
+putchar(digit1 + '0');
+putchar(digit2 + '0');
+
+if (digit1 != 9 || digit2 != 9)
+{	    
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
