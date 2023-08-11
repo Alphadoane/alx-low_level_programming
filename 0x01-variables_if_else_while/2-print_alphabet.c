671487169627b2656f090cb1aa06e7b65ae30ccc
Alphadoane <superalphablood@gmail.com>
2023-08-11 03:58:51 -0700

    s

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-    char letter = 'a';
+char letter = 'a';
     
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
 
-    putchar('\n');
+putchar('\n');
     
-    return (0);
+return (0);
 }
 
