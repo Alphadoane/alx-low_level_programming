f9e50ef73b4ea57fd2aa4d6c745a035c4b522b4f
Alphadoane <superalphablood@gmail.com>
2023-08-11 07:17:07 -0700

    s

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,16 +9,12 @@
  */
 int main(void)
 {
-putchar('0' + 0);
-putchar('0' + 1);
-putchar('0' + 2);
-putchar('0' + 3);
-putchar('0' + 4);
-putchar('0' + 5);
-putchar('0' + 6);
-putchar('0' + 7);
-putchar('0' + 8);
-putchar('0' + 9);
+int number;
+for (number = 0; number <= 9; number++)
+{
+putchar(number + '0');
+}
+
 putchar('\n');
 
 return (0);
