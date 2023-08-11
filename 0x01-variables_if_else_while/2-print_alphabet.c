59c2eada6f46396f6b16d06b5e72ee4de577a165
Alphadoane <superalphablood@gmail.com>
2023-08-11 04:31:55 -0700

    s

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the alphabet in lowercase,
+ *              followed by a new line.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char letter = 'a';
