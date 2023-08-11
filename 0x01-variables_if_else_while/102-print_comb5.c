c821fe43d22852380ba111e4ae61b489c8627ab1
Alphadoane <superalphablood@gmail.com>
2023-08-11 06:18:06 -0700

    s

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints combinations of two two-digit numbers
+ *              in ascending order, with numbers separated by a space and a comma.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num1;
+int num2;
+
+for (num1 = 0; num1 <= 98; num1++)
+{
+for (num2 = num1 + 1; num2 <= 99; num2++)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
