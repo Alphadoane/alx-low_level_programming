e6785e5f9566531806f299ed3644a8f584b7c3f0
Alphadoane <superalphablood@gmail.com>
2023-08-15 04:12:02 -0700

    d

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 /*
  *main - Entry point of the program
+ *
  *Description: the program prints putchar and returns '0' when successful
- */
+ *
+ * */
 int main(void)
 {
+/*
+ * prints putchar as an output
+ * */
 printf("_putchar\n");
 return (0);
 }
