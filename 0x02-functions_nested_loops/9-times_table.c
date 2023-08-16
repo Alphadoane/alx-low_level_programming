a2ac829ed0f41be826b4ae876486b07184f7fff2
Alphadoane <superalphablood@gmail.com>
2023-08-16 06:25:59 -0700

    s

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+int row, column, result;
+
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+result = row * column;
+
+if (column == 0)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+if (result < 10)
+{
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(' ');
+}
+
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+
+if (column < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('\n');
+}
+}
+}
+}
+}
