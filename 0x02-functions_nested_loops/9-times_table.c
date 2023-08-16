1a17542441058dcc9bc9bf487bd3fde6d8354c21
Alphadoane <superalphablood@gmail.com>
2023-08-16 06:34:08 -0700

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,49 +1,42 @@
 #include "main.h"
 
 /**
+ * print_number - Prints a two-digit number with appropriate formatting
+ * @num: The number to be printed
+ */
+void print_number(int num)
+{
+if (num < 10)
+_putchar(' ');
+else
+_putchar(num / 10 + '0');
+
+_putchar(num % 10 + '0');
+}
+
+/**
  * times_table - Prints the 9 times table
  */
 void times_table(void)
 {
-int row, column, result;
+int row, column;
 
 for (row = 0; row <= 9; row++)
 {
 for (column = 0; column <= 9; column++)
 {
-result = row * column;
+int result = row * column;
 
-if (column == 0)
-{
-_putchar('0');
-_putchar(',');
-_putchar(' ');
-}
-else
-{
-if (result < 10)
-{
-_putchar(' ');
-_putchar(' ');
-}
-else
-{
-_putchar(' ');
-}
-
-_putchar(result / 10 + '0');
-_putchar(result % 10 + '0');
-
-if (column < 9)
+if (column != 0)
 {
 _putchar(',');
 _putchar(' ');
 }
-else
-{
+
+print_number(result);
+
+if (column == 9)
 _putchar('\n');
-}
-}
 }
 }
 }
