65ed5060254c17e117a7df5cd2e44c2901d9933d
Alphadoane <superalphablood@gmail.com>
2023-08-16 03:05:37 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -31,15 +31,3 @@ int main(void)
 print_alphabet();
 return (0);
 }
-
-/**
- * _putchar - Writes a character to the standard output
- * @c: The character to be written
- *
- * Return: On success, the character written is returned. On error, -1 is
- * returned.
- */
-int _putchar(char c)
-{
-return write(1, &c, 1);
-}
