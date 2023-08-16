381ab6b68d3f1a95f7e0e1573dc1a9007f3ea7d4
Alphadoane <superalphablood@gmail.com>
2023-08-16 05:36:25 -0700

    s

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,99 +4,6 @@
 /* Function to print a character */
 int _putchar(char c);
 
-/* Function to print the lowercase alphabet */
-void print_alphabet(void);
-
-/* Function to print a character */
-int _putchar(char c);
-
-/* Function to print the lowercase alphabet 10 times */
-void print_alphabet_x10(void);
-
-/* Function to print a character */
-int _putchar(char c);
-
-/* Function to check if a character is lowercase */
-int _islower(int c);
-
-/* Function to print a character */
-int _putchar(char c);
-
-/* Function to check if a character is lowercase */
-int _islower(int c);
-
-/* Function to check if a character is an alphabetic letter */
-int _isalpha(int c);
-
-/* Function to print a character */
-int _putchar(char c);
-
-/* Function to check if a character is lowercase */
-int _islower(int c);
-
-/* Function to check if a character is an alphabetic letter */
-int _isalpha(int c);
-
-/* Function to print the sign of a number */
-int print_sign(int n);
-
-/* Function to print a character */
-int _putchar(char c);
-
-/* Function to check if a character is lowercase */
-int _islower(int c);
-
-/* Function to check if a character is an alphabetic letter */
-int _isalpha(int c);
-
-/* Function to print the sign of a number */
-int print_sign(int n);
-
-/* Function to compute the absolute value of an integer */
-int _abs(int n);
-
-/* Function to print a character */
-int _putchar(char c);
-
-/* Function to check if a character is lowercase */
-int _islower(int c);
-
-/* Function to check if a character is an alphabetic letter */
-int _isalpha(int c);
-
-/* Function to print the sign of a number */
-int print_sign(int n);
-
-/* Function to compute the absolute value of an integer */
-int _abs(int n);
-
-/* Function to print the last digit of a number */
-int print_last_digit(int n);
-
-/* Function to print a character */
-int _putchar(char c);
-
-/* Function to check if a character is lowercase */
-int _islower(int c);
-
-/* Function to check if a character is an alphabetic letter */
-int _isalpha(int c);
-
-/* Function to print the sign of a number */
-int print_sign(int n);
-
-/* Function to compute the absolute value of an integer */
-int _abs(int n);
-
-/* Function to print the last digit of a number */
-int print_last_digit(int n);
-
-/* Function to print every minute of the day of Jack Bauer */
-void jack_bauer(void);
-
-/* Function to print a character */
-int _putchar(char c);
-
 /* Function to check if a character is lowercase */
 int _islower(int c);
 
@@ -119,3 +26,4 @@ void jack_bauer(void);
 void times_table(void);
 
 #endif /* MAIN_H */
+
