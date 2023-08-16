3e35da1168b37202cefcb68a088a678cde8a390e
Alphadoane <superalphablood@gmail.com>
2023-08-16 04:56:37 -0700

    s

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -73,4 +73,25 @@ int _abs(int n);
 /* Function to print the last digit of a number */
 int print_last_digit(int n);
 
+/* Function to print a character */
+int _putchar(char c);
+
+/* Function to check if a character is lowercase */
+int _islower(int c);
+
+/* Function to check if a character is an alphabetic letter */
+int _isalpha(int c);
+
+/* Function to print the sign of a number */
+int print_sign(int n);
+
+/* Function to compute the absolute value of an integer */
+int _abs(int n);
+
+/* Function to print the last digit of a number */
+int print_last_digit(int n);
+
+/* Function to print every minute of the day of Jack Bauer */
+void jack_bauer(void);
+
 #endif /* MAIN_H */
