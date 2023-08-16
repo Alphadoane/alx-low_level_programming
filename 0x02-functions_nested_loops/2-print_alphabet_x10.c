95a1f416d6e17931a3cc5e638576b48b5bb9e0b7
Alphadoane <superalphablood@gmail.com>
2023-08-16 04:14:18 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int count;
+
+for (count = 0; count < 10; count++)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+}
