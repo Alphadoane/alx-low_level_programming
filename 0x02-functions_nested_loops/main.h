cf3f4c4521b2e315a63711fb7e155d902755044d
Alphadoane <superalphablood@gmail.com>
2023-08-16 04:17:07 -0700

    s

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,14 @@ void print_alphabet(void);
 
 #endif /* MAIN_H */
 
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function to print a character */
+int _putchar(char c);
+
+/* Function to print the lowercase alphabet 10 times */
+void print_alphabet_x10(void);
+
+#endif /* MAIN_H */
+
