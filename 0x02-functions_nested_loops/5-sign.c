06313bc424dd34ac2bcbb8546ac85983b136af41
Alphadoane <superalphablood@gmail.com>
2023-08-16 04:33:13 -0700

    s

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,8 @@
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ * Return: 1 if n is greater than zero, 
+ 0 if n is zero, -1 if n is less than zero
  */
 int print_sign(int n)
 {
