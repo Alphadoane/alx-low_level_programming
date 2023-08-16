9515876bf8cadc23f14a871efe732160adb18aa5
Alphadoane <superalphablood@gmail.com>
2023-08-16 02:52:21 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,24 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates the usage of the print_alphabet
+ * function. The main function returns 0 to indicate successful execution.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ *
+ * Description: This function uses the _putchar function to print each
+ * lowercase letter of the alphabet, followed by a new line character.
  */
 void print_alphabet(void)
 {
@@ -9,24 +26,20 @@ char letter = 'a';
 
 while (letter <= 'z')
 {
-putchar(letter);
+_putchar(letter);
 letter++;
 }
-
-putchar('\n');
+_putchar('\n');
 }
 
 /**
-* main - Entry point of the program.
-*
-* Description: This program demonstrates the use of the print_alphabet function
-* to print the lowercase alphabet followed by a new line. It then returns 0 to
-* indicate successful execution.
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, the character written is returned. On error, -1 is
+ * returned.
+ */
+int _putchar(char c)
 {
-print_alphabet();
-return (0);
+return write(1, &c, 1);
 }
