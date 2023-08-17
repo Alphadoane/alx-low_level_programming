1d68d50dbbf86451637da0776f9ce03bae95f6da
Alphadoane <superalphablood@gmail.com>
2023-08-17 07:47:37 -0700

    s

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,10 @@
  */
 void print_number(int n)
 {
+int divisor = 1;
+int temp = n;
+int digit;
+
 if (n == 0)
 {
 _putchar('0');
@@ -16,21 +20,20 @@ if (n < 0)
 {
 _putchar('-');
 n = -n;
+temp = n;
 }
 
-int divisor = 1;
-int temp = n;
-
-while (temp / 10 != 0)
+while (temp > 9)
 {
 divisor *= 10;
 temp /= 10;
 }
 
-while (divisor != 0)
+while (divisor > 0)
 {
-_putchar('0' + n / divisor);
-n %= divisor;
+digit = n / divisor;
+_putchar('0' + digit);
+n -= digit * divisor;
 divisor /= 10;
 }
 }
