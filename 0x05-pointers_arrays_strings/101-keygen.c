8dfc99cb596aa4de156f18c6bf662c071d37b652
Alphadoane <superalphablood@gmail.com>
2023-08-21 03:00:22 -0700

    key generation code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,39 +12,39 @@ int main(void)
 char password[100];
 int i, length, sum, random;
 
-srand(time(NULL)); // Seed the random number generator with the current time
+srand(time(NULL));
 
-length = rand() % 11 + 6; // Generate a random password length between 6 and 16 characters
+length = rand() % 11 + 6;
 
 sum = 0;
 for (i = 0; i < length; i++)
 {
-random = rand() % 62; // Generate a random number between 0 and 61
+random = rand() % 62;
 
-if (random < 26) // If random is in the range [0, 25], choose a lowercase letter
+if (random < 26)
 {
 password[i] = 'a' + random;
 }
-else if (random < 52) // If random is in the range [26, 51], choose an uppercase letter
+else if (random < 52)
 {
 password[i] = 'A' + (random - 26);
 }
-else // If random is in the range [52, 61], choose a digit
+else
 {
 password[i] = '0' + (random - 52);
 }
 
-sum += password[i]; // Calculate the sum of ASCII values for characters in the password
+sum += password[i];
 }
 
-if (sum < 2772) // Ensure the sum of ASCII values is at least 2772 (as required by 101-crackme)
+if (sum < 2772)
 {
-password[i - 1] += 2772 - sum; // Adjust the last character to meet the requirement
+password[i - 1] += 2772 - sum;
 }
 
-password[length] = '\0'; // Null-terminate the password
+password[length] = '\0';
 
-printf("%s\n", password); // Print the generated password
+printf("%s\n", password);
 
 return (0);
 }
