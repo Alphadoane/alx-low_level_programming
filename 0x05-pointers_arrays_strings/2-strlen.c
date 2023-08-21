6b9fe50ecb904a46de5c9ca7ba9159ce1e76a916
Alphadoane <superalphablood@gmail.com>
2023-08-21 00:01:40 -0700

    third array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to a string (null-terminated character array).
+ *
+ * Return: The length of the string (number of characters).
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+return length;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,8 @@ void reset_to_98(int *n);
 /* Function prototype for swap_int function */
 void swap_int(int *a, int *b);
 
+/* Function prototype for _strlen function */
+int _strlen(char *s);
+
 #endif /* MAIN_H */
 
