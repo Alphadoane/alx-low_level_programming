09f17aace9e667e7f3bd1f78b255903fc8be9128
Alphadoane <superalphablood@gmail.com>
2023-08-30 02:56:35 -0700

    hello c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Declaration of the _putchar function */
+int _putchar(char c);
+
+/* Declaration of the _puts_recursion function */
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
+
