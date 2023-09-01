9f9f4341ac427f579e051dd1a2c09e9761128d31
Alphadoane <superalphablood@gmail.com>
2023-09-01 06:16:22 -0700

    d

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,15 +5,14 @@
  * @argc: Argument count (number of command-line arguments)
  * @argv: Argument vector (array of command-line argument strings)
  *
- * Description: This function is the entry point of the program.
- * It calculates and prints the number of command-line arguments passed into it,
- * excluding the program name itself (argv[0]).
+ * Description:calculates and prints the number of command-line arguments
+ * passed into it,excluding the program name itself (argv[0]).
  *
  * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-int num_arguments = argc -1;
+int num_arguments = argc - 1;
 /* Print the value of argc (number of command-line arguments) */
 printf("%d\n", argc - 1);
 
