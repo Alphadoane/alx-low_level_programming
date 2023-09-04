6015271cdc666b849d1dfa3d801323116d9e23e7
Alphadoane <superalphablood@gmail.com>
2023-09-03 21:54:49 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,6 +3,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *arr;
+unsigned int i;
 
 if (size == 0)
 return (NULL);
@@ -12,7 +13,7 @@ arr = malloc(size * sizeof(char));
 if (arr == NULL)
 return (NULL);
 
-for (unsigned int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 arr[i] = c;
 }
