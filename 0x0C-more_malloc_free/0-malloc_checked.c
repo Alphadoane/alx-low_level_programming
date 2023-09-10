931aedfefc44a6a6c41c7adc3cfcd16009499177
Alphadoane <superalphablood@gmail.com>
2023-09-10 14:38:00 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 /**
  * malloc_checked - Allocates memory using malloc.
  * @b: The number of bytes to allocate.
@@ -16,6 +17,26 @@ if (ptr == NULL)
 exit(98); /* Terminate the program with status 98 */
 }
 
-return (ptr);
+int main(void)
+{
+char *c;
+int *i;
+float *f;
+double *d;
+
+c = malloc_checked(sizeof(char) * 1024);
+printf("%p\n", (void *)c);
+i = malloc_checked(sizeof(int) * 402);
+printf("%p\n", (void *)i);
+f = malloc_checked(sizeof(float) * 100000000);
+printf("%p\n", (void *)f);
+d = malloc_checked(sizeof(double) * 10);
+printf("%p\n", (void *)d);
+free(c);
+free(i);
+free(f);
+free(d);
+}
+return (0);
 }
 
