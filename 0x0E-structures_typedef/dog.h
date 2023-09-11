e72aaada09c06757b3c8d4e415529191d2565f0b
Alphadoane <superalphablood@gmail.com>
2023-09-11 07:24:44 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,15 @@ float age; /* the age of the dog in years */
 char *owner; /* the name of the dog's owner */
 };
 /* Other declarations or function prototypes if needed */
+/**
+ * @brief Initializes a dog structure with the provided data.
+ *
+ * @param d     Pointer to the struct dog to be initialized.
+ * @param name  The name of the dog.
+ * @param age   The age of the dog.
+ * @param owner The name of the dog's owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* DOG_H */
 
