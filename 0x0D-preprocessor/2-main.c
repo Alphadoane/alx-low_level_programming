a57033b5c7a111050449b388d47c4f64ca3460a2
Alphadoane <superalphablood@gmail.com>
2023-09-11 05:47:05 -0700

    2 main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the name of the source file.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 printf("%s\n", __FILE__);
