f2d03c40ec9f2c9e6c11f90ebbe7c7434dfa569e
Alphadoane <superalphablood@gmail.com>
2023-09-11 07:33:29 -0700

    free dogd

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * @brief Frees memory allocated for a dog structure.
+ *
+ * @param d Pointer to the dog structure to be freed.
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+if (d->name != NULL)
+{
+free(d->name);
+}
+if (d->owner != NULL)
+{
+free(d->owner);
+}
+free(d);
+}
+}
