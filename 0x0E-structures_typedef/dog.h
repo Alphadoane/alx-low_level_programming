c9dfb9a8b46e35044572f39dc2efecb579f00c7d
Alphadoane <superalphablood@gmail.com>
2023-09-11 06:57:00 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/* Structure representing a dog with its name, age, and owner.*/
 struct dog
 {
-char *name;
-float age;
-char *owner;
+char *name; /* the name of the dog */
+float age; /* the age of the dog in years */
+char *owner; /* the name of the dog's owner */
 };
 
 #endif /* DOG_H */
