003093725f355c938088a4687fb67c4e743ca20a
Alphadoane <superalphablood@gmail.com>
2023-09-13 04:26:27 -0700

    hello

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * print_name - Prints a name using a provided function.
+ * @name: The name to be printed.
+ * @f: A function pointer that defines how the name is printed.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+{
+f(name);
+}
+}
+
+/**
+ * print_uppercase - Prints a name in uppercase.
+ * @name: The name to be printed.
+ */
+void print_uppercase(char *name)
+{
+while (*name != '\0')
+{
+putchar(toupper(*name));
+name++;
+}
+putchar('\n');
+}
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+char *myName = "John";
+printf("Original name: %s\n", myName);
+print_name(myName, print_uppercase);
+return (0);
+}
