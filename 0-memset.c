54d48d30fbc665d40511c420664e7f93285527b5
Alphadoane <superalphablood@gmail.com>
2024-07-08 02:14:18 -0700

    hello

--- 0-memset.c
+++ 0-memset.c
@@ -1,11 +1,12 @@
+#include "main.h"
+
 /**
- * _memset - fills the first n bytes of the memory area pointed to by s
- * with the constant byte b.
+ * _memset - fills memory with a constant byte
  * @s: pointer to the memory area to be filled
  * @b: the constant byte to fill the memory with
- * @n: number of bytes to fill
+ * @n: the number of bytes to be filled
  *
- * Return: a pointer to the memory area s
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -17,5 +18,5 @@ s[i] = b;
 }
 
 return (s);
-
 }
+
