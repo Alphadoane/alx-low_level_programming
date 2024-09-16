3e2fc9cf9bc6af318ade9122e668ab2962e62a6d
Alphadoane <superalphablood@gmail.com>
2024-09-16 12:33:51 -0700

    w

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,6 +22,30 @@ return (count);
 }
 
 /**
+ * allocate_word - Allocates memory for a word and copies the word into it.
+ * @str: The string containing the word.
+ * @start: The start index of the word in the string.
+ * @end: The end index of the word in the string.
+ *
+ * Return: A pointer to the newly allocated word, or NULL on failure.
+ */
+char *allocate_word(char *str, int start, int end)
+{
+char *word;
+int i;
+
+word = malloc((end - start + 1) * sizeof(char));
+if (word == NULL)
+return (NULL);
+
+for (i = 0; start < end; start++, i++)
+word[i] = str[start];
+word[i] = '\0';
+
+return (word);
+}
+
+/**
  * strtow - Splits a string into words.
  * @str: The string to split.
  *
@@ -30,7 +54,7 @@ return (count);
 char **strtow(char *str)
 {
 char **words;
-int i, j, k, word_len, wc, start;
+int wc, i = 0, k = 0, start, end;
 
 if (str == NULL || *str == '\0')
 return (NULL);
@@ -39,44 +63,34 @@ wc = word_count(str);
 if (wc == 0)
 return (NULL);
 
-/* Allocate memory for the array of words */
 words = malloc((wc + 1) * sizeof(char *));
 if (words == NULL)
 return (NULL);
 
-i = 0, k = 0;
 while (str[i] != '\0')
 {
-/* Skip leading spaces */
 while (str[i] == ' ')
 i++;
 
 if (str[i] == '\0')
 break;
 
-/* Find the length of the next word */
 start = i;
 while (str[i] != '\0' && str[i] != ' ')
 i++;
-word_len = i - start;
+end = i;
 
-/* Allocate memory for the word and copy it */
-words[k] = malloc((word_len + 1) * sizeof(char));
+words[k] = allocate_word(str, start, end);
 if (words[k] == NULL)
 {
-/* Free previously allocated memory on failure */
-for (j = 0; j < k; j++)
-free(words[j]);
+for (i = 0; i < k; i++)
+free(words[i]);
 free(words);
 return (NULL);
 }
-
-for (j = 0; j < word_len; j++)
-words[k][j] = str[start + j];
-words[k][j] = '\0'; /* Null-terminate the word */
 k++;
 }
+words[k] = NULL;
 
-words[k] = NULL; /* Null-terminate the array of words */
 return (words);
 }
