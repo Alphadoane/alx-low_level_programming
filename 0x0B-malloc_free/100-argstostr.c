2580a4765e6465025783a63f566ca532d7ceb069
Alphadoane <superalphablood@gmail.com>
2024-09-16 11:56:11 -0700

    w

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,38 +10,37 @@
  */
 char *argstostr(int ac, char **av)
 {
-    char *result;
-    int total_len = 0;
-    int i, j, k;
+char *result;
+int total_len = 0;
+int i, j, k;
 
-    if (ac == 0 || av == NULL)
-        return NULL;
+if (ac == 0 || av == NULL)
+return NULL;
 
-    /* Calculate total length required for the new string */
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            total_len++;
-        total_len++; /* For newline character */
-    }
-
-    /* Allocate memory for the new string */
-    result = (char *)malloc((total_len + 1) * sizeof(char)); /* +1 for the null terminator */
-    if (result == NULL)
-        return NULL;
-
-    /* Copy arguments into the new string */
-    k = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            result[k++] = av[i][j];
-        }
-        result[k++] = '\n';
-    }
-    result[k] = '\0'; /* Null-terminate the string */
-
-    return result;
+/* Calculate total length required for the new string */
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+total_len++;
+total_len++; /* For newline character */
 }
 
+/* Allocate memory for the new string */
+result = (char *)malloc((total_len + 1) * sizeof(char)); /* +1 for the null terminator */
+if (result == NULL)
+return NULL;
+
+/* Copy arguments into the new string */
+k = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+result[k++] = av[i][j];
+}
+result[k++] = '\n';
+}
+result[k] = '\0'; /* Null-terminate the string */
+
+return result;
+}
